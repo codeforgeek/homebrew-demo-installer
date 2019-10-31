88e92c77309f8d40fde2916e0e58b2f304854668
Shahid Shaikh <UnixRoot@Shahids-MacBook-Air.local>
2019-10-31 12:36:31 +0530

    push

--- demo-installer.rb
+++ demo-installer.rb
@@ -3,7 +3,7 @@ class DemoInstaller < Formula
   homepage "https://github.com/codeforgeek/demo-installer/"
   url "https://github.com/codeforgeek/demo-installer/archive/v1.0.0.tar.gz"
   sha256 "520b428ac8025c3efabbd6370164af472abdebf3427cadeed296c8bcb604b2c2"
-  depends_on "node@v11.15.0"
+  depends_on "node@11.15.0"
   depends_on "redis"
 
   def install
