1b03a68c183f0d717fc057a705135537e6f65a3c
Shahid Shaikh <rwtc66@gmail.com>
2019-10-31 14:14:42 +0530

    Update demo-installer.rb

--- demo-installer.rb
+++ demo-installer.rb
@@ -2,7 +2,7 @@ class DemoInstaller < Formula
   desc "demo installer for our IPFS POC"
   homepage "https://github.com/codeforgeek/demo-installer/"
   url "https://github.com/codeforgeek/demo-installer/archive/v1.0.1.tar.gz"
-  sha256 "520b428ac8025c3efabbd6370164af472abdebf3427cadeed296c8bcb604b2c2"
+  sha256 "2c767bc2e85dd2fcd4c424131774736371c4118ac79b22e04e6c4f5428b2e975"
   depends_on "node@10"
   depends_on "redis"
   depends_on "wget"
